f4f631522d41f4a77b15ece815a194723eedb881
Jiin <jiinkim02@gmail.com>
2021-08-06 17:01:10 -0700

    add educate and enact screens

--- navigation/HomeStack.js
+++ navigation/HomeStack.js
@@ -1,37 +1,17 @@
 import React from "react";
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
-import HomeScreen from "../screens/HomeScreen";
-import ExtraScreen from "../screens/ExtraScreen";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import EnactScreen from "../screens/EnactScreen";
+import EducateScreen from "../screens/EducateScreen";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+``;
 
 export default function HomeStack() {
-  const Tab = createMaterialBottomTabNavigator();
+  const Tab = createMaterialTopTabNavigator();
   return (
     <Tab.Navigator initialRouteName="Home">
-      <Tab.Screen
-        name="Extra"
-        component={ExtraScreen}
-        options={{
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons
-              name="file-question"
-              color={color}
-              size={26}
-            />
-          ),
-        }}
-      />
-
-      <Tab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="home" color={color} size={26} />
-          ),
-        }}
-      />
+      <Tab.Screen name="Educate" component={EducateScreen} />
+      <Tab.Screen name="Enact" component={EnactScreen} />
     </Tab.Navigator>
   );
 }
--- navigation/index.js
+++ navigation/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { AuthProvider } from "./AuthProvider";
 import NavigationStack from "./NavigationStack";
+
 export default function Providers() {
   return (
     <AuthProvider>

e4b74d7c640b82f6ec3cf1eee04e5f0d58be7be3
Archisha Datta <archisha.datta@gmail.com>
2021-08-08 01:12:55 -0700

    email rep proof of concept

--- screens/Enact/GenerateLinktree.js
+++ screens/Enact/GenerateLinktree.js
@@ -4,6 +4,9 @@ import { Text, TouchableOpacity, View, SafeAreaView } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { useContext, useEffect } from "react/cjs/react.development";
 import { AuthContext } from "../../navigation/AuthProvider.js";
+import global from "../../styles.js";
+import { Ionicons } from "@expo/vector-icons";
+
 
 export default function GenerateLinktree(props) {
   const navigation = useNavigation();
@@ -12,16 +15,22 @@ export default function GenerateLinktree(props) {
     setDisplay(false);
   }, []);
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{backgroundColor:'white', height:'100%'}}>
       <TouchableOpacity
         onPress={() => {
           setDisplay(true);
           navigation.goBack();
         }}
+        style={{height: 40, width:40, position: 'absolute', top: 0, left: 10}}
+        hitSlop={{top: 20, bottom: 20, left: 20, right: 20}}
       >
-        <Text>◂</Text>
+          <View>
+            <Ionicons name="chevron-back-outline" size={25} color="black" />
+          </View>
       </TouchableOpacity>
-      <Text>Generate Linktree</Text>
+      <View style={{ flex: 1, margin: 20, marginTop: 50 }}>
+      <Text style={global.pageTitle}>GenerateLinktree</Text>
+      </View>
     </SafeAreaView>
   );
 }
